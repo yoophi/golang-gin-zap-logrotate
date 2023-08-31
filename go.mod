89f1ab4e21c3f0ea1359437f4426b6737688937c
Pyunghyuk Yoo <pyunghyuk.yoo@payhere.in>
2023-08-31 13:17:41 +0900

    feat: logrotate and move old logs to some directory

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/gin-contrib/zap v0.1.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-co-op/gocron v1.33.0
 	github.com/utahta/go-cronowriter v1.2.0
 	go.uber.org/zap v1.25.0
 )
@@ -18,6 +19,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -27,10 +29,12 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.opentelemetry.io/otel v1.10.0 // indirect
 	go.opentelemetry.io/otel/trace v1.10.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
 	"time"
 
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"github.com/go-co-op/gocron"
 	writer "github.com/utahta/go-cronowriter"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 func main() {
-	baseDir := "./logs"
-	w1 := writer.MustNew(baseDir+"/example.log.%Y%m%d_%H%M",
+	logDir := "./logs"
+	archiveDir := "./archived"
+
+	w1 := writer.MustNew(logDir+"/example.log.%Y%m%d_%H%M",
 		writer.WithInit(), writer.WithMutex(),
 	)
-	w2 := writer.MustNew(baseDir+"/internal_error.log.%Y%m%d_%H%M",
+	w2 := writer.MustNew(logDir+"/internal_error.log.%Y%m%d_%H%M",
 		writer.WithInit(), writer.WithMutex(),
 	)
 	logger := zap.New(
@@ -28,11 +34,55 @@ func main() {
 		zap.ErrorOutput(zapcore.AddSync(w2)),
 	)
 
-	// ---
+	logger.Info("hello, world!")
+
+	s := gocron.NewScheduler(time.UTC)
+
+	job, err := s.Every("1m").Do(func() {
+		sourceDir := logDir
+		destDir := archiveDir
+		pattern := "example.log.*"
+
+		err := os.MkdirAll(destDir, 0755)
+		if err != nil {
+			logger.Error(fmt.Sprintf("mkdir failed: %+v", err))
+			return
+		}
+
+		matches, err := filepath.Glob(filepath.Join(sourceDir, pattern))
+		if err != nil {
+			logger.Error(fmt.Sprintf("error matching files: %+v", err))
+			return
+		}
+
+		sort.Slice(matches, func(i, j int) bool {
+			return matches[i] > matches[j]
+		})
+
+		for i, match := range matches {
+			if i == 0 {
+				fmt.Println("first element:", match)
+				continue
+			}
+			fileName := filepath.Base(match)
+			destFilePath := filepath.Join(destDir, fileName)
+			fmt.Println("$ mv", fileName, destFilePath)
+
+			err := os.Rename(match, destFilePath)
+			if err != nil {
+				logger.Error(fmt.Sprintf("error moving file: %s", match))
+			} else {
+				logger.Info(fmt.Sprintf("moved %s to %s", match, destFilePath))
+			}
+		}
+	})
+	fmt.Printf("%+v\n", job)
+	if err != nil {
+		return
+	}
+	s.StartAsync()
+
 	r := gin.New()
-
-	//logger, _ := zap.NewProduction()
-
 	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
 	r.Use(ginzap.RecoveryWithZap(logger, true))
 
